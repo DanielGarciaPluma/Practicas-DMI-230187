deb890a8d648c0843eabc5f579379da745eed60a
Edgar cabrera <230496@utxicotepec.edu.mx>
2026-09-11 16:20:32 -0600

    actualizacion de la practica
    Se siguio con la practica de contador suma, resta y reseteo - Diseño de la pantalla

--- hello_world_app/lib/main.dart
+++ hello_world_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:hello_world_app/Presentation/Screens/counter/counter_screens.dart';
+import 'package:hello_world_app/Presentation/Screens/counter/counter_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CounterScreens()
+      home: CounterScreen()
     );
   }
 }

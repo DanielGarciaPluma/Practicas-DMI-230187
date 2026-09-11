e5a17efd5af83f12d41fb8db8a8ce62f3e8d9a68
Edgar cabrera <230496@utxicotepec.edu.mx>
2026-09-11 16:37:07 -0600

    actualizacion de la practica
    Se siguio con la practica de contador suma, resta y reseteo - Diseño de la pantalla

--- hello_world_app/lib/Presentation/Screens/counter/counter_screen.dart
+++ hello_world_app/lib/Presentation/Screens/counter/counter_screen.dart
@@ -1,46 +1,64 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatefulWidget {
-  const CounterScreen({super.key});
+class CounterFunctionsScreen extends StatefulWidget {
+  const CounterFunctionsScreen({super.key});
 
   @override
-  State<CounterScreen> createState() => _CounterScreenState();
+  State<CounterFunctionsScreen> createState() => _CounterFunctionsScreenState();
 }
 
-class _CounterScreenState extends State<CounterScreen> {
+class _CounterFunctionsScreenState extends State<CounterFunctionsScreen> {
   int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Counter Screen'),
+        title: const Text('Counter Functions'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh_rounded),
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            }, 
+          ),
+        ],
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('$clickCounter',
-                style: const TextStyle(
-                    fontSize: 160, fontWeight: FontWeight.w100)),
-                    
-            Text('Click${ clickCounter == 1 ? '':'s' }', style: const TextStyle(fontSize: 25))
-
-            // if( clickCounter == 1 )
-            //   const Text('Click', style: TextStyle(fontSize: 25)),
-
-            // if( clickCounter != 1 )
-            //   const Text('Clicks', style: TextStyle(fontSize: 25)),
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('$clickCounter',
+                style: const TextStyle( 
+                  fontSize: 160, fontWeight: FontWeight.w100 )),
+              Text('Click${clickCounter == 1 ? '' : 's' }', style: TextStyle( fontSize: 25 ))
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          clickCounter++;
-          setState(() {});
-        },
-        child: const Icon(Icons.plus_one),
-      ),
-    );
+        floatingActionButton: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                clickCounter ++;
+                setState(() {});
+              },
+              child: const Icon( Icons.plus_one ),
+            ),
+            
+            SizedBox(height: 15,),
+
+            FloatingActionButton(
+              onPressed: () {
+                clickCounter --;
+                setState(() {});
+              },
+              child: const Icon( Icons.exposure_minus_1_outlined ),
+            ),
+          ],
+        )
+      );
   }
 }
